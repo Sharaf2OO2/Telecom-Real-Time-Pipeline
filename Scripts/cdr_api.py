d78410f53b6ccb15d1a7c1a039df8fdbb9819bee
Sharaf Ahmed <sharafahmed2002@gmail.com>
2025-09-27 00:58:31 +0300

    Fix duration range in CDR generation to allow 1 second minimum

--- Scripts/cdr_api.py
+++ Scripts/cdr_api.py
@@ -11,7 +11,7 @@ def generate_cdr():
     tower_id = r.choice(TOWER_IDS)
     call_type = r.choice(CALL_TYPES)
     start = datetime.utcnow()
-    duration = r.randint(5, 300)    #5s-5min
+    duration = r.randint(1, 300)    #1s-5min
     end = start + timedelta(seconds=duration)
     
     return {
